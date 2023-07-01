8a89a7ad618c654508f248f5e60dd0b75f55c102
kotobukid <kotobuki@daqua.jp>
2023-07-01 23:36:48 +0900

    tokioでasync fn トレイト境界を試す

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use tokio;
+
+#[tokio::main]
+async fn main() {
+    println!("start!");
+    println!("result is {}", add_async_after_1sec(1, 1).await);
+}
+
+async fn add_async_after_1sec<T>(a: T, b: T) -> T
+    where T: std::ops::Add<Output=T> {
+    tokio::time::sleep(std::time::Duration::from_secs(1)).await;
+    a + b
 }
