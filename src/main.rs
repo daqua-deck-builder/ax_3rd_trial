af68ca656cda88fb85e1be2f82fb37ef15b0d702
kotobukid <kotobuki@daqua.jp>
2023-07-02 01:13:08 +0900

    クッキーをパースする

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,6 @@ use axum::{
     response::IntoResponse,
     routing::{
         get,
-        post,
     },
     Router,
     Server,
@@ -18,6 +17,7 @@ use axum::{
 use serde::{
     Deserialize
 };
+use axum_extra::extract::cookie::CookieJar;
 
 #[tokio::main]
 async fn main() {
@@ -25,9 +25,9 @@ async fn main() {
 
     let app = Router::new()
         .route("/", get(home_handler))
-        .route("/q", get(query_parse_handler)
-            .post(body_parse_handler),
-        )
+        .route("/q", get(query_parse_handler).post(body_parse_handler))
+        .route("/c", get(cookie_parse_handler))
+        // .route("/both", post(q_and_body))
         ;
 
     println!("{}", &addr);
@@ -42,7 +42,7 @@ async fn home_handler() -> impl IntoResponse {
     (StatusCode::OK, "Index page")
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Clone, Copy, Default)]
 struct QuerySample1 {
     id: i32,
 }
@@ -55,4 +55,21 @@ async fn query_parse_handler(query: Query<QuerySample1>) -> impl IntoResponse {
 async fn body_parse_handler(body: Json<QuerySample1>) -> impl IntoResponse {
     println!("{:?}", body);
     (StatusCode::OK, format!("id is {}", body.id))
+}
+
+// fn q_and_body((body, query): (Json<QuerySample1>, Query<QuerySample1>)) -> impl IntoResponse {
+//     async move {
+//         println!("{:?}", body);
+//         println!("{:?}", query);
+//         (StatusCode::OK, format!("id is {}, {}", body.id, query.id))
+//     }
+// }
+
+async fn cookie_parse_handler(cookie: CookieJar) -> impl IntoResponse {
+    println!("{:?}", cookie);
+    if let Some(sid) = cookie.get("sid") {
+        (StatusCode::OK, format!("id is {}", sid))
+    } else {
+        (StatusCode::UNAUTHORIZED, "not authorized".into())
+    }
 }
