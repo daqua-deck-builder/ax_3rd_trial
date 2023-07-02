a55c84c02e4f3a47390addf5656c81e39b768ac4
kotobukid <kotobuki@daqua.jp>
2023-07-02 21:23:02 +0900

    Extractorの受け取り順序を変えたらコンパイルが通った

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use axum::{
     response::IntoResponse,
     routing::{
         get,
+        post,
     },
     Router,
     Server,
@@ -28,7 +29,7 @@ async fn main() {
         .route("/q", get(query_parse_handler).post(body_parse_handler))
         .route("/c", get(cookie_parse_handler))
         .route("/cq", get(cookie_and_query))
-        // .route("/both", post(q_and_body))
+        .route("/both", post(q_and_body))
         ;
 
     println!("{}", &addr);
@@ -58,13 +59,12 @@ async fn body_parse_handler(body: Json<QuerySample1>) -> impl IntoResponse {
     (StatusCode::OK, format!("id is {}", body.id))
 }
 
-// fn q_and_body((body, query): (Json<QuerySample1>, Query<QuerySample1>)) -> impl IntoResponse {
-//     async move {
-//         println!("{:?}", body);
-//         println!("{:?}", query);
-//         (StatusCode::OK, format!("id is {}, {}", body.id, query.id))
-//     }
-// }
+// async fn q_and_body(body: Json<QuerySample1>, query: Query<QuerySample1>) -> impl IntoResponse { // compile error
+async fn q_and_body(query: Query<QuerySample1>, body: Json<QuerySample1>) -> impl IntoResponse {
+    println!("{:?}", body);
+    println!("{:?}", query);
+    (StatusCode::OK, format!("id is {}, {}", body.id, query.id))
+}
 
 async fn cookie_parse_handler(cookie: CookieJar) -> impl IntoResponse {
     println!("{:?}", cookie);
