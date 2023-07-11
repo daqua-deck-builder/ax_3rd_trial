01b681a4e700a6f146f7a6ad00673c10c9b49e36
kotobukid <kotobuki@daqua.jp>
2023-07-11 23:06:03 +0900

    intoでもOK

--- examples/ref_vec.rs
+++ examples/ref_vec.rs
@@ -2,9 +2,12 @@ fn main() {
     let v = vec![1, 2, 3, 4, 5];
     let vv = &v[1..3];
     let mut vvc = vv.to_vec();
+    let mut vvcc: Vec<i32> = vv.into();
     vvc.push(100);
+    vvcc.push(200);
 
     println!("{:?}", v);   // 参照されただけなのでvの束縛は健在
     println!("{:?}", vv);   // to_vecでは新規にVecを作るのでvvの束縛は健在
-    println!("{:?}", vvc)
+    println!("{:?}", vvc);
+    println!("{:?}", vvcc)
 }
