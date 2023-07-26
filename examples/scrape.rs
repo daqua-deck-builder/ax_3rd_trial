3af51967fcd51e8ea4012b5a741e62be8b377dcd
kotobukid <kotobuki@daqua.jp>
2023-07-27 01:12:49 +0900

    カードリストの各ページへのリンク部分取得

--- examples/scrape.rs
+++ examples/scrape.rs
@@ -6,6 +6,7 @@ use std::fs;
 use std::path::{Path, PathBuf};
 use std::fs::File;
 use std::io::prelude::*;
+use async_recursion::async_recursion;
 
 #[derive(Clone)]
 struct SearchQuery {
@@ -25,12 +26,12 @@ enum ProductType {
 }
 
 impl SearchQuery {
-    fn new(product_no: String, product_type: ProductType, card_page: i32) -> SearchQuery {
+    fn new(product_no: &String, product_type: ProductType, card_page: i32) -> SearchQuery {
         SearchQuery {
             search: "".into(),
             keyword: "".into(),
             product_type,
-            product_no,
+            product_no: product_no.clone(),
             card_page: card_page.to_string(),
             card_kind: "".into(),
             rarelity: "".into(),
@@ -86,19 +87,22 @@ fn try_mkdir(rel_path: &Path) -> Result<(), std::io::Error> {
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     try_mkdir(Path::new("./text_cache")).unwrap();
-    simple_request().await.unwrap();
+    simple_request(&String::from("WXi-14"), 1).await.unwrap();
 
     Ok(())
 }
 
-pub async fn simple_request() -> Result<(), reqwest::Error> {
+#[async_recursion]
+pub async fn simple_request(product_no: &String, card_page: i32) -> Result<(), reqwest::Error> {
+    println!("{} {}", product_no, card_page);
+
     let url = "https://www.takaratomy.co.jp/products/wixoss/card/card_list.php";
 
-    let search_query: SearchQuery = SearchQuery::new(String::from("WXi-14"), ProductType::Booster, 1);
+    let search_query: SearchQuery = SearchQuery::new(&product_no, ProductType::Booster, card_page);
 
-    match search_query.cache_check("./text_cache".to_string()) {
-        Ok(content) => {
-            println!("{}", content);
+    let main: Option<String> = match search_query.cache_check("./text_cache".to_string()) {
+        Ok(content_) => {
+            Some(content_)
         }
         _ => {
             let form: HashMap<String, String> = search_query.to_hashmap();
@@ -116,19 +120,61 @@ pub async fn simple_request() -> Result<(), reqwest::Error> {
             if let Some(parent_path) = cache_filename.parent() {
                 try_mkdir(&parent_path).unwrap();
 
-                let document: Html = Html::parse_document(&body);
-                let main_selector: Selector = Selector::parse("#dipThum").unwrap();
+                let content = find_one(&body, ".cardDip".into());
 
-                let file: Result<File, std::io::Error> = File::create(&cache_filename);
-                if let Ok(mut file_) = file {
-                    for element in document.select(&main_selector) {
-                        println!("{}", element.inner_html());
-                        file_.write_all(element.inner_html().as_bytes()).unwrap();
+                if let Some(element) = &content {
+                    let file: Result<File, std::io::Error> = File::create(&cache_filename);
+                    if let Ok(mut file_) = file {
+                        file_.write_all(element.as_bytes()).unwrap();
                     }
                 }
+                content
+            } else {
+                None
             }
         }
+    };
+
+    if let Some(count) = find_one(&main.unwrap(), "h3 p span".into()) {
+        let count = extract_number(&count);
+
+        if let Some(count) = count {
+            let pages = (count / 21) + 1;
+
+            if card_page < pages {
+                simple_request(&product_no, card_page + 1).await.unwrap();
+            }
+        }
+    } else {
+        println!("not found");
     }
 
+
     Ok(())
+}
+
+fn find_one(content: &String, selector: String) -> Option<String> {
+    let document: Html = Html::parse_document(&content);
+    let main_selector: Selector = Selector::parse(selector.as_str()).unwrap();
+
+    if let Some(element) = document.select(&main_selector).next() {
+        Some(element.inner_html())
+    } else {
+        None
+    }
+}
+
+fn find_many(content: &String, selector: String) -> Vec<String> {
+    let document: Html = Html::parse_document(&content);
+    let main_selector: Selector = Selector::parse(selector.as_str()).unwrap();
+    let mut result: Vec<String> = Vec::new();
+    for element in document.select(&main_selector) {
+        result.push(element.inner_html());
+    }
+    result
+}
+
+fn extract_number(s: &String) -> Option<i32> {
+    let digits: String = s.chars().filter(|c| c.is_digit(10)).collect();
+    digits.parse().ok()
 }
